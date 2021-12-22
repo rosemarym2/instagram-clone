d92d86e9412dc27938db5fee3535fc78c3c72c35
rosemarym2 <rosemarymay91@gmail.com>
2021-12-22 09:53:59 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import { PostCreator } from "./components/postCreator";
+import { fetchRequest } from "./components/postCreator";
 
 const App = () => {
   const [user, setUser] = useState(); //item on left is the value, item on right is the function (e.g. user =name of value, setUser = function)
@@ -8,11 +8,15 @@ const App = () => {
   const [list, setList] = useState([]);
   const [data, setData] = useState([]);
   const [error, setError] = useState();
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setList([...list, post]);
-  }
+    fetchRequest(username, email, password, setUser);
+    //the user sends a fetch request from the front-end to the back-end to create a new user
+  };
 
   useEffect(() => {
     fetchPictures();
@@ -32,7 +36,7 @@ const App = () => {
         throw new Error("the error is...cannot fetch pictures")
       }
       const data = await response.json();
-        setData(data);
+        setData(data);//fetching the data/instagram placeholder pictures
     } catch (e) {
       setError({ error: true, message: e.message})
     }
@@ -40,14 +44,22 @@ const App = () => {
 
   return (
     <div className = "App">
-      <h1>{user ? `Welcome ${user}`: "Please sign in"}</h1>
-      <input onChange={(e) => setUser(e.target.value)} />
-      <PostCreator setPost={setPost} submitHandler={submitHandler}/>
-      {list.map((item, index) => {
-        return <p key={index}>{item}</p>
-      })}
+      <h1>{user ? `Welcome ${user}` : "Please sign up"}</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Submit</button>
+      </form>
       {data.map((item, index) => {
-        return <img key={index} alt= "Outdoors landscape" src={item.download_url} />
+        return <img key={index} alt= "Outdoors landscape" src={item.download_url} />//download_url taken from API as it contains the picture
       })}
     </div>
   );
@@ -56,3 +68,62 @@ const App = () => {
 
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+// import './App.css';
+// import { PostCreator } from "./components/postCreator";
+
+// const App = () => {
+//   const [user, setUser] = useState(); //item on left is the value, item on right is the function (e.g. user =name of value, setUser = function)
+//   const [post, setPost] = useState();
+//   const [list, setList] = useState([]);
+//   const [data, setData] = useState([]);
+//   const [error, setError] = useState();
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+//     setList([...list, post]);
+//   }
+
+//   useEffect(() => {
+//     fetchPictures();
+//   }, []);
+  
+
+//   const fetchPictures = async () => {
+//     try {
+//       const response = await fetch("https://picsum.photos/v2/list?page=2&limit=10", {
+//         method: "GET",
+//         headers: {
+//           "mode": "no-cors",
+//         }
+//       });
+//       console.log(response);
+//       if(response.status !== 200){
+//         throw new Error("the error is...cannot fetch pictures")
+//       }
+//       const data = await response.json();
+//         setData(data);//fetching the data/instagram placeholder pictures
+//     } catch (e) {
+//       setError({ error: true, message: e.message})
+//     }
+//   };
+
+//   return (
+//     <div className = "App">
+//       <h1>{user ? `Welcome ${user}`: "Please sign in"}</h1>
+//       <input onChange={(e) => setUser(e.target.value)} />
+//       <PostCreator setPost={setPost} submitHandler={submitHandler}/>
+//       {list.map((item, index) => {
+//         return <p key={index}>{item}</p>
+//       })}
+//       {data.map((item, index) => {
+//         return <img key={index} alt= "Outdoors landscape" src={item.download_url} />//download_url taken from API as it contains the picture
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+// export default App;
--- src/components/postCreator/index.js
+++ src/components/postCreator/index.js
@@ -1,13 +1,30 @@
-import { useEffect } from "react";
+export const fetchRequest = async (username, email, password, setUser) => {
+    try {
+      const response = await fetch("http://localhost:5001/user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      setUser(data.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export const PostCreator = ({setPost, submitHandler}) => {
-    return (
-        <form onSubmit={submitHandler, useEffect}>
-        <input onChange={(e) => setPost(e.target.value)} />
-        <button type="submit">Submit</button>
-        </form>
-    );
-};
 
+// import { useEffect } from "react";
 
+// export const PostCreator = ({setPost, submitHandler}) => {
+//     return (
+//         <form onSubmit={submitHandler, useEffect}>
+//         <input onChange={(e) => setPost(e.target.value)} />
+//         <button type="submit">Submit</button>
+//         </form>
+//     );
+// };
         
